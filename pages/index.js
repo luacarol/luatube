f39568c98dc98433a3c8ca4f1902a820ae475053
luacarol <lucarolp.deoli@gmail.com>
2022-11-09 09:21:38 -0300

    Criando a ideia de chamar componentes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
 function HomePage() {
-    return <div>Welcome to Next.js</div>
+    const estilosDaHomePage = { backgroundColor: "red" }
+
+    return (
+        <div style={estilosDaHomePage}>
+            <Menu />
+            <Header />
+            <Timeline />
+        </div>
+    )
 }
 
 export default HomePage
+
+function Menu() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+function Timeline() {
+    return (
+        <div>
+            Timeline
+        </div>
+    )
+}
